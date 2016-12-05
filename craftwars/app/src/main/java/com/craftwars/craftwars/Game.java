e82cf9fa186b100795d646fedacd45e0b0430620
Marcin Szczepanski <marcin_szczepanski@epam.com>
2016-12-05 13:31:20 +0100

    added game, gamepanel and threading

--- craftwars/app/src/main/java/com/craftwars/craftwars/Game.java
+++ craftwars/app/src/main/java/com/craftwars/craftwars/Game.java
@@ -2,12 +2,20 @@ package com.craftwars.craftwars;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class Game extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
+        //turn title off
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //set to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+//        setContentView(R.layout.activity_game);
+        setContentView(new GamePanel(this));
     }
 }
